7c3ee3f0a267036baa88ab93cf9e7666810d26bb
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-30 19:24:29 -0300

    tests for get specified subject with professors

--- tests/integration/subject.test.ts
+++ tests/integration/subject.test.ts
@@ -4,7 +4,9 @@ import app from "../../src/app";
 import { cleanDatabase, endConnection, startConnection } from "../utils/database";
 import { populateCategories } from "../factories/categoryFactory";
 import { populateSemesters } from "../factories/semesterFactory";
-import { populateSubjects } from "../factories/subjectFactory";
+import { createSubjectProfessorRelation, populateSubjects } from "../factories/subjectFactory";
+import { populateProfessors } from "../factories/professorFactory";
+import Subject from "../../src/entities/Subject";
 
 beforeAll(startConnection);
 afterAll(endConnection);
@@ -12,7 +14,7 @@ beforeEach(cleanDatabase)
 
 describe("GET /subjects", () => {
   it("should answer status 200 from valid url", async () => {
-    const length = await populateSubjects();
+    await populateSubjects();
     const result = await supertest(app).get("/subjects");
     expect(result.status).toEqual(200);
   });
@@ -26,3 +28,31 @@ describe("GET /subjects", () => {
     expect(result.status).toEqual(404);
   });
 });
+
+describe("GET /subjects/:id/professors", () => {
+    it("should answer status 404 from inexistent id", async () => {
+        const subjectLength = await populateSubjects();
+        const result = await supertest(app).get(`/subjects/${subjectLength+1}/professors`);
+        expect(result.status).toEqual(404);
+    });
+    it("should answer status 400 from invalid id", async () => {
+        const result = await supertest(app).get(`/subjects/-1/professors`);
+        expect(result.status).toEqual(400);
+    });
+    it("should answer a object with professor array from valid id", async () => {
+        await populateSubjects();
+        await populateProfessors();
+        await createSubjectProfessorRelation(1,1)
+        const result = await supertest(app).get(`/subjects/1/professors`);
+        console.log()
+        expect(result.body.professors.length).toEqual(1);
+      });
+      it("should answer an object from valid id", async () => {
+        await populateSubjects();
+        await populateProfessors();
+        const subject= await createSubjectProfessorRelation(1,1)
+        const result = await supertest(app).get("/subjects/1/professors");
+
+        expect(result.body).toMatchObject(subject)
+      });
+  });

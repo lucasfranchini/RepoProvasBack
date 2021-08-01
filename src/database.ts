6f363be7256c1a7f542f08e8fedb248477689d8d
lucasfranchini <lucasfranchini.a@gmail.com>
2021-08-01 02:40:45 -0300

    change database to get url from .env

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ export default async function connect () {
   const connection = connectionManager.create({
     name: "default",
     type: "postgres",
-    url: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+    url: process.env.DATABASE_URL,
     entities: ["src/entities/*.ts"]
   });
   await connection.connect();
